f55f160baa0a555f7e10ffb48a48846dab1fb8ae
palanivel annadurai <palanivel.annadurai@npcompete.org>
2021-04-21 11:25:02 +0530

    intrest calculate changes

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function compute() {
+    if (document.getElementById("principal").value) {
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + parseInt(years);
 
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear() + parseInt(years);
-
-    document.getElementById("result").innerHTML = "If you deposit <span class='highlighted'>" + principal + "</span>,\<br\>at an interest rate of <span class='highlighted'>" + rate + "%</span>,\<br\>You will receive an amount of <span class='highlighted'>" + interest + "</span>,\<br\>in the year <span class='highlighted'>" + year + "</span>\<br\>"
+        document.getElementById("result").innerHTML = "If you deposit <span class='highlighted'>" + principal + "</span>,\<br\>at an interest rate of <span class='highlighted'>" + rate + "%</span>,\<br\>You will receive an amount of <span class='highlighted'>" + interest + "</span>,\<br\>in the year <span class='highlighted'>" + year + "</span>\<br\>"
+    } else {
+        alert('Enter a positive number');
+    }
 }
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval + '%';
 }
 
-function getPrincipalVal() {debugger
+function getPrincipalVal() {
+    debugger
     var principal = document.getElementById("principal").value;
     if (principal === '0' || principal.includes('-')) {
         alert('Enter a positive number');
